8d473bce88b191f09b70761ff17f6146b720b43d
PradhumnSrivastava <srivastavapradhumn@gmail.com>
2026-03-20 14:07:03 +0530

    Add Messages in Chatbot

--- chatmodels/chatbot.py
+++ chatmodels/chatbot.py
@@ -1,5 +1,6 @@
 from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
 from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.messages import HumanMessage, AIMessage, SystemMessage
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -11,19 +12,18 @@ llm = HuggingFaceEndpoint(repo_id="meta-llama/Meta-Llama-3-8B-Instruct",
 
 model = ChatHuggingFace(llm=llm)
 
-template = ChatPromptTemplate.from_messages([
-    ("human", "Explain {input} in simple terms.")
-])
 
-#message = []
+messages = [SystemMessage(content="You are a helpful assistant that explains topics in simple terms.")
+             ]
 
 while True:
     input_text = input("Enter a topic to explain (or 'exit' to quit): ")
     
     if input_text.lower() == 'exit':
         break
-    
-    messages = template.format_messages(input=input_text)
+    messages.append(HumanMessage(content=input_text))
 
     response = model.invoke(messages)
+    
+    messages.append(AIMessage(content=response.content))
     print(f"AI: {response.content}")
